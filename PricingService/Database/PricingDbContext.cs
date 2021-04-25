be2b7e96a58e21731f048bfbb5d58a9a8eec65e8
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 21:09:45 +0200

    Update PricingDbContext

--- PricingService/Database/PricingDbContext.cs
+++ PricingService/Database/PricingDbContext.cs
@@ -9,13 +9,25 @@ namespace PricingService.Database
 {
     public class PricingDbContext : DbContext
     {
-        public DbSet<BasePriceModel> BasePrices { get; set; }
+        public DbSet<CustomerModel> Customers { get; set; }
+        public DbSet<ServiceModel> Services { get; set; }
         public DbSet<DiscountModel> Discounts { get; set; }
         public DbSet<FreeDaysModel> FreeDays { get; set; }
+        public DbSet<ServiceDefinitionModel> ServiceDefinitions { get; set; }
+        public DbSet<DayOfWeekModel> Days { get; set; }
 
         public PricingDbContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DayOfWeekModel>()
+                .Property(x => x.Day)
+                .HasConversion<int>();
         }
     }
 }
