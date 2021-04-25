0c9c63ccc17f3a90f084cc0badbba512dcfb6f7f
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 21:09:54 +0200

    Add DatabaseSeeder class

---
+++ PricingService/Database/DatabaseSeeder.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PricingService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PricingService.Database
+{
+    public class DatabaseSeeder
+    {
+        public void Seed(PricingDbContext dbContext)
+        {
+            DayOfWeekModel[] billingDaysA = new[]
+            {
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Monday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Tuesday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Wednesday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Thursday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Friday
+                },
+            };
+
+            DayOfWeekModel[] billingDaysB = new[]
+            {
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Monday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Tuesday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Wednesday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Thursday
+                },
+                new DayOfWeekModel()
+                {
+                    Day = DayOfWeek.Friday
+                },
+            };
+
+            ServiceDefinitionModel[] serviceDefinitions = new[] {
+                new ServiceDefinitionModel()
+                {
+                    ServiceName = "ServiceA",
+                    Price = 0.2,
+                    BillingDays = new List<DayOfWeekModel>(billingDaysA)
+                },
+                new ServiceDefinitionModel()
+                {
+                    ServiceName = "ServiceB",
+                    Price = 0.24,
+                    BillingDays = new List<DayOfWeekModel>(billingDaysB)
+                },
+                new ServiceDefinitionModel()
+                {
+                    ServiceName = "ServiceC",
+                    Price = 0.4
+                }
+            };
+
+            ServiceDefinitionModel[] oldServiceDefinitions = dbContext.ServiceDefinitions.Include(x => x.BillingDays).ToArray().Where(x => x.GetType() == typeof(ServiceDefinitionModel)).ToArray();
+            DayOfWeekModel[] oldDays = oldServiceDefinitions.SelectMany(x => x.BillingDays).ToArray();
+            dbContext.Days.RemoveRange(oldDays);
+            dbContext.ServiceDefinitions.RemoveRange(oldServiceDefinitions);
+            dbContext.ServiceDefinitions.AddRange(serviceDefinitions);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
