d2668c5a97240a10cd1ffc820a14e5b29de29f87
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 21:08:56 +0200

    Add calculator classes

---
+++ PricingService/Calculations/DayCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PricingService.Calculations
+{
+    public static class DayCalculator
+    {
+        public static int GetDays(DateTime from, DateTime to, DayOfWeek[] days = null)
+        {
+            int daysBetween = (int)(to - from).TotalDays;
+
+            if (days != null && days.Length > 0)
+            {
+                daysBetween = Enumerable.Range(0, daysBetween).Count(x => from.AddDays(x).DayOfWeek.In(days));
+            }
+
+            return daysBetween;
+        }
+
+        private static bool In(this DayOfWeek source, DayOfWeek[] validDays)
+        {
+            return validDays.Contains(source);
+        }
+    }
+}
---
+++ PricingService/Calculations/PriceCalculator.cs
@@ -0,0 +1,61 @@
+﻿using PricingService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PricingService.Calculations
+{
+    public static class PriceCalculator
+    {
+        public static double GetPrice(CustomerModel customer, DateTime startDate, DateTime endDate)
+        {
+            double finalPrice = 0;
+            int freeDays = customer.FreeDays != null ? customer.FreeDays.FreeDays : 0;
+
+            if (((endDate - startDate).TotalDays - freeDays) <= 0)
+            {
+                if ((endDate - startDate).TotalDays <= 0)
+                {
+                    throw new Exception("Cannot calculate service price. The end date was evaluated to occur before the start date.");
+                }
+
+                return 0;
+            }
+
+            foreach (ServiceModel service in customer.UsedServices)
+            {
+                DateTime from = service.StartDate != null && DateTime.Compare(startDate, (DateTime)service.StartDate) > 0 ? startDate : (DateTime)service.StartDate;
+                DateTime to = service.EndDate != null && DateTime.Compare(endDate, (DateTime)service.EndDate) > 0 ? (DateTime)service.EndDate : endDate;
+
+                if (DateTime.Compare(to, from) < 0)
+                {
+                    throw new Exception("Cannot calculate service price. The end date was evaluated to occur before the start date.");
+                }
+
+                int daysUsed = DayCalculator.GetDays(from, to, service.BillingDays.Select(x => x.Day).ToArray()) - freeDays;
+
+                if (daysUsed <= 0)
+                {
+                    freeDays -= daysUsed;
+                    continue;
+                }
+                else if (freeDays > 0)
+                {
+                    freeDays = 0;
+                }
+
+                finalPrice += GetServicePrice(daysUsed, service);
+            }
+
+            return finalPrice;
+        }
+
+        private static double GetServicePrice(int usedDays, ServiceModel service)
+        {
+            double discountFactor = service.Discount != null ? service.Discount.DiscountPercent / 100.0 : 0;
+
+            return service.Price * usedDays * (1 - discountFactor);
+        }
+    }
+}
