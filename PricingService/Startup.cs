0d87941f8fc89c092416e99b1f6301132a8a2234
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 12:40:42 +0200

    Register database context in Startup.cs

--- PricingService/Startup.cs
+++ PricingService/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PricingService.Database;
 
 namespace PricingService
 {
@@ -27,6 +29,10 @@ namespace PricingService
         {
             services.AddControllers();
             services.AddLogging();
+            services.AddDbContext<PricingDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
