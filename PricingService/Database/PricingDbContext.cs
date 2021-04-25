b67c48cc6fe278bf5235b719ca8d441fb0083e7a
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 12:40:27 +0200

    Add database context

---
+++ PricingService/Database/PricingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PricingService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PricingService.Database
+{
+    public class PricingDbContext : DbContext
+    {
+        public DbSet<BasePriceModel> BasePrices { get; set; }
+        public DbSet<DiscountModel> Discounts { get; set; }
+        public DbSet<FreeDaysModel> FreeDays { get; set; }
+    }
+}
