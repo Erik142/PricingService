50041409fd869853eb585aeb26532e873eae01b0
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 21:10:32 +0200

    Seed database on application start

--- PricingService/Program.cs
+++ PricingService/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PricingService.Extensions;
 
 namespace PricingService
 {
@@ -13,7 +14,7 @@ namespace PricingService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().SeedData().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
