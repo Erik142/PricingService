29f2542982a0b4af1e5592391731d300765b2b8c
Erik Wahlberger <EWahlberger@gmail.com>
2021-04-25 21:10:05 +0200

    Add IHostExtensions class

---
+++ PricingService/Extensions/IHostExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PricingService.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PricingService.Extensions
+{
+    public static class IHostExtensions
+    {
+        public static IHost SeedData(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetService<PricingDbContext>();
+
+                DatabaseSeeder seeder = new DatabaseSeeder();
+                seeder.Seed(dbContext);
+            }
+
+            return host;
+        }
+    }
+}
